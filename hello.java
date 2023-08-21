71bd783fc44dabba09e68dca7b0ea672286caee4
HQuang123 <huynguyensteph@gmail.com>
2023-08-21 15:49:47 +0700

    no commet

---
+++ hello.java
@@ -0,0 +1,7 @@
+// This is a comment
+class HelloWorld {
+    public static void main(String[] args) {
+        // This is another comment
+        System.out.println("Hello World"); // This prints Hello World
+    }
+}
